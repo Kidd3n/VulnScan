0da7ca137f94516180eb1dac3e770ffe13a4bd95
Ruben Gomez <santiagogomezgarro@gmail.com>
2024-02-26 21:10:41 -0600

    VulnScan

--- VulnScan.py
+++ VulnScan.py
@@ -2,10 +2,10 @@ import nmap
 
 def scan_network(network):
     nm = nmap.PortScanner()
-    nm.scan(hosts=network, arguments='-sn')  
+    nm.scan(hosts=network, arguments='-sn')
 
     for host in nm.all_hosts():
-        print('[+] IP: %s' % host)
+        print('\n[+] IP: %s' % host)
         if 'hostnames' in nm[host]:
             print('[+] Nombre: %s' % nm[host]['hostnames'][0]['name'])
         print('[+] Estado: %s' % nm[host].state())
@@ -19,7 +19,7 @@ def scan_network(network):
                 print('[+] Fabricante: %s' % vendor_name)
 
         print('')
-
+        
 if __name__ == "__main__":
     network_address = input("\n[?] IP de la red local (Ej: 192.168.1.0): ")
     scan_network(network_address + '/24')

92b98c3c2128d8c21d532074f01bf9912b59f1ac
Ruben Gomez <santiagogomezgarro@gmail.com>
2024-02-27 20:22:48 -0600

    scapy

--- VulnScan.py
+++ VulnScan.py
@@ -1,25 +1,25 @@
+from scapy.all import ARP, Ether, srp
+import manuf
 import nmap
 
+def get_oui(mac_address):
+    parser = manuf.MacParser()
+    oui = parser.get_manuf(mac_address)
+    return oui if oui else "Desconocido"
+
 def scan_network(network):
-    nm = nmap.PortScanner()
-    nm.scan(hosts=network, arguments='-sn')
+    arp_request = ARP(pdst=network)
+    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-    for host in nm.all_hosts():
-        print('\n[+] IP: %s' % host)
-        if 'hostnames' in nm[host]:
-            print('[+] Nombre: %s' % nm[host]['hostnames'][0]['name'])
-        print('[+] Estado: %s' % nm[host].state())
+    print("\n[+] IP\t\t\t[+] MAC Address\t\t[+] Fabricante\n------------------------------------------------------------")
+    for element in answered_list:
+        ip_address = element[1].psrc
+        mac_address = element[1].hwsrc
+        oui = get_oui(mac_address)
+        print(f"{ip_address}\t\t{mac_address}\t  {oui}")
 
-        if 'mac' in nm[host]['addresses']:
-            print('[+] Direccion MAC: %s' % nm[host]['addresses']['mac'])
-        if 'vendor' in nm[host]:
-            vendors = nm[host]['vendor']
-            if vendors:
-                vendor_name = list(vendors.values())[0]
-                print('[+] Fabricante: %s' % vendor_name)
-
-        print('')
-        
 if __name__ == "__main__":
     network_address = input("\n[?] IP de la red local (Ej: 192.168.1.0): ")
-    scan_network(network_address + '/24')
+    scan_network(network_address + "/24")
